7cf4b1a8784f807883154cd93a1638348d35ac79
WANGHAO <1727386617@qq.com>
2019-07-19 10:35:38 +0800

    Update BaseExceptionHandler.java

--- src/main/java/com/hao0129/cloud/common/exception/BaseExceptionHandler.java
+++ src/main/java/com/hao0129/cloud/common/exception/BaseExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.hao0129.cloud.common.exception;
 
+import com.hao0129.cloud.common.util.RenderUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class BaseExceptionHandler extends RuntimeException{
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseExceptionHandler.class);
+
+    @Autowired
+    protected RenderUtil render;
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public String ExceptionHandler(Exception e){
+        String message = e.getMessage();
+        logger.debug("Exception:{}",message);
+        e.printStackTrace();
+        return render.failMsg(e.getMessage());
+    }
 }
